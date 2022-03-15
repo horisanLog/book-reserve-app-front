178fe8e63a2e483a38a80cd726801cfc66b0886b
horisanLog <ttie7374@gmail.com>
2022-03-16 06:49:31 +0900

    トップページ

--- src/ui/pages/index.tsx
+++ src/ui/pages/index.tsx
@@ -1,15 +1,19 @@
 import { memo, VFC } from "react";
 import { useTodo } from "../../hooks/todo"
 import { Todo } from "../../domain/todo"
+import { TodoForm } from "../components/Lv3/TodoForm"
 
-export const TopPage: VFC = memo(() => {
-  const todo = useTodo()
+export const TopPage: React.FC = memo(() => {
+  const { loading, todo, changeTodo } = useTodo()
+
+  if (loading) {
+    return <div>{"ローディング中"}</div>
+  }
   
   return (
     <>
       <h1>TOP</h1>
-      <input name="text" type="text" value={todo.todo.text} onChange={(e) => todo.changeTodo(todo.todo, e)} ></input>
-      {todo.todo.text}
+      <TodoForm todo={todo} changeTodo={changeTodo} />
       <br />
     </>
   );
