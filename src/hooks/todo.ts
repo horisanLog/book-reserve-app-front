2bacaea1030b9f7bd63d9b9c7e3e914ae86ec348
horisanLog <ttie7374@gmail.com>
2022-03-16 06:49:42 +0900

    Update todo.ts

--- src/hooks/todo.ts
+++ src/hooks/todo.ts
@@ -1,23 +1,40 @@
-import React, { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from 'react'
 import { Todo } from "../domain/todo";
+import { helloRequest } from "../api/helloRequest"
 
 const data: Todo = {
   text: "",
   count: 0,
 };
 
+
 export const useTodo = () => {
   const [todo, setTodo] = useState<Todo>(data);
-  console.log("test")
+  const [loading, setLoading] = useState(true);
 
   const changeTodo = (todo: Todo, e: React.ChangeEvent<HTMLInputElement>) => {
     switch (e.target.name) {
-      case "text":
+      case "title":
         return setTodo({ ...todo, text: e.target.value });
     }
-
-    // return { ...todo, todo };
   };
 
-  return { todo, changeTodo };
+  const fetchTodo = useCallback(async () => {
+    try {
+      const response = await helloRequest()
+      if (response.status === 200) {
+        setTodo(response.data)
+      }
+      // eslint-disable-next-line
+    } catch (error: any) {
+      console.log("error")
+    }
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    fetchTodo()
+  }, [fetchTodo])
+
+  return { loading, todo, changeTodo };
 };
