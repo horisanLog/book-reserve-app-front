d047664c556e6003e24e403f5e1bffbf37348732
horisanLog <ttie7374@gmail.com>
2022-03-22 20:02:44 +0900

    リセットCSSの設定

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import { Routing } from "./router";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import CssBaseline from '@mui/material/CssBaseline';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <BrowserRouter>
       <Routing />
     </BrowserRouter>
