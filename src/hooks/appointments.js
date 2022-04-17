0335550c8604cb8c45fcb5b8610f81b10b54789d
horisanLog <ttie7374@gmail.com>
2022-04-17 15:45:41 +0900

    scheduleForm

---
+++ src/hooks/appointments.js
@@ -0,0 +1,81 @@
+export const appointments = [
+  {
+    title: 'MTG',
+    startDate: new Date(2022, 5, 25, 9, 35),
+    endDate: new Date(2022, 5, 25, 11, 30),
+    id: 0,
+    location: 'Room 1',
+  }, {
+    title: '昼会',
+    startDate: new Date(2022, 5, 25, 12, 11),
+    endDate: new Date(2022, 5, 25, 13, 0),
+    id: 1,
+    location: 'Room 1',
+  }, {
+    title: 'こんにちは',
+    startDate: new Date(2022, 5, 25, 14, 30),
+    endDate: new Date(2022, 5, 25, 15, 35),
+    id: 2,
+    location: 'Room 2',
+  }, {
+    title: 'デート',
+    startDate: new Date(2022, 5, 26, 10, 0),
+    endDate: new Date(2022, 5, 26, 11, 0),
+    id: 3,
+    location: 'Room 2',
+  }, {
+    title: '飲み会',
+    startDate: new Date(2022, 5, 26, 12, 0),
+    endDate: new Date(2022, 5, 26, 13, 35),
+    id: 4,
+    location: 'Room 2',
+  }, {
+    title: '',
+    startDate: new Date(2022, 5, 26, 14, 30),
+    endDate: new Date(2022, 5, 26, 15, 45),
+    id: 5,
+    location: 'Room 2',
+  }, {
+    title: 'ダラダラ',
+    startDate: new Date(2022, 5, 27, 9, 45),
+    endDate: new Date(2022, 5, 27, 11, 15),
+    id: 6,
+    location: 'Room 1',
+  }, {
+    title: '遊び',
+    startDate: new Date(2022, 5, 27, 12, 0),
+    endDate: new Date(2022, 5, 27, 14, 0),
+    id: 7,
+    location: 'Room 3',
+  }, {
+    title: '買い物',
+    startDate: new Date(2022, 5, 27, 15, 15),
+    endDate: new Date(2022, 5, 27, 16, 30),
+    id: 8,
+    location: 'Room 3',
+  }, {
+    title: '面談',
+    startDate: new Date(2022, 5, 28, 11, 0),
+    endDate: new Date(2022, 5, 28, 12, 0),
+    id: 9,
+    location: 'Room 3',
+  }, {
+    title: '試験',
+    startDate: new Date(2022, 5, 28, 11, 0),
+    endDate: new Date(2022, 5, 28, 13, 30),
+    id: 10,
+    location: 'Room 1',
+  }, {
+    title: 'レビュー',
+    startDate: new Date(2022, 5, 28, 14, 0),
+    endDate: new Date(2022, 5, 28, 15, 30),
+    id: 11,
+    location: 'Room 2',
+  }, {
+    title: '誕生日',
+    startDate: new Date(2022, 5, 26, 0, 0),
+    endDate: new Date(2022, 5, 27, 0, 0),
+    id: 34,
+    location: 'Room 1',
+  }, 
+];
